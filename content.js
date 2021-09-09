4134d4568f0b054eda7280f3ccc197f6752e6711
lizhenyu <775087535@qq.com>
2021-09-10 00:37:16 +0800

    update

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 hideElement('.readerTopBar')
 hideElement('.readerControls')
-
-toggleFullScreen()
+changeFont()
+// toggleFullScreen()
 
 function hideElement(className) {
   const $el = document.querySelector(className)
@@ -16,4 +16,34 @@ function toggleFullScreen() {
       document.exitFullscreen()
     }
   }
+}
+
+// 改变宽度
+function changeContentWidth() {
+  const item = document.querySelector(".readerContent .app_content");
+  const currentValue = getCurrentMaxWidth(item);
+  item.style['max-width'] = currentValue + 1 + 'px';
+  const myEvent = new Event('resize');
+  window.dispatchEvent(myEvent)
+}
+
+function getCurrentMaxWidth(element) {
+  let currentValue = window.getComputedStyle(element).maxWidth;
+  currentValue = currentValue.substring(0, currentValue.indexOf('px'));
+  currentValue = parseInt(currentValue);
+  return currentValue;
+}
+
+// 修改字体
+function changeFont() {
+  const styles = `
+    * {
+      font-family: 'TsangerJinKai01-9128 W02' !important;;
+    }
+  `
+  const styleSheet = document.createElement('style')
+  styleSheet.type = 'text/css'
+  styleSheet.innerText = styles
+  document.head.appendChild(styleSheet)
+  changeContentWidth()
 }
